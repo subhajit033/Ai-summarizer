b40136bd82350f5129353f930357dd021934ccd7
Subhajit Kundu <kundusubhajit73@gmail.com>
2023-07-14 09:13:59 +0530

    setting up redux store for api calling

---
+++ src/services/article.js
@@ -0,0 +1,33 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+//     params: {
+//       url: 'https://time.com/6266679/musk-ai-open-letter/',
+//       length: '3'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': '9dc79ea5c8mshe313fe98b1ebd74p13ae7ejsne5f253de8390',
+//       'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+//     }
+//   };
+export const articleApi = createApi({
+    reducerPath: "articleApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+        prepareHeaders: (headers)=>{
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+            return headers;
+        }
+    }),
+    endpoints: (builder)=>({
+        getSummary: builder.query({
+            query: (params)=> `test`
+        })
+    })
+
+})
